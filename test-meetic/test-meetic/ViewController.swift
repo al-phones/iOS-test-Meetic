7e6e1e5abd27dce1ff4567414206302aab84262c
Alex Phonesavanh <aphonesavanh@vente-privee.com>
2018-12-16 16:08:34 +0100

    Init project

---
+++ test-meetic/test-meetic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-meetic
+//
+//  Created by Alex Phonesavanh on 16/12/2018.
+//  Copyright © 2018 aphones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
